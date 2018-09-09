749c1992d09cb502e8e169aeefab232daa790d8c
Isaac Truett <itruett@gmail.com>
2018-09-09 19:47:37 +0000

    Adds a control to open/close all sections of a guide. Also only displays the list of blog references if the guide is referenced by at least one blog.

--- s.js
+++ s.js
@@ -1,16 +1,41 @@
-function addCloseButtonsToDetails() {
+function enableGuideControls() {
 	var details = document.getElementsByTagName('details');
 	for (var i = 0; i < details.length; i++) {
-		var button = document.createElement('button');
-		button.innerHTML = 'Close';
-		button.onclick = function() {
-				this.parentNode.removeAttribute('open');
-				this.parentNode.scrollIntoView({
-						behavior: "smooth",
-						block: "center",
-						inline: "nearest"});};
-		details[i].appendChild(button);
+		if (details[i].id != 'open-all') {
+			var button = document.createElement('button');
+			button.innerHTML = 'Close';
+			button.onclick = function() {
+					this.parentNode.removeAttribute('open');
+					this.parentNode.scrollIntoView({
+							behavior: "smooth",
+							block: "center",
+							inline: "nearest"});};
+			details[i].appendChild(button);
+		}
+	}
+
+	var openAll = document.getElementById('open-all');
+	openAll.onclick = function(element) {
+		var details = document.getElementsByTagName('details');
+
+		for (var i = 0; i < details.length; i++) {
+			if (details[i].id != 'open-all') {
+				if (openAll.open) {
+					details[i].removeAttribute('open');
+				} else {
+					details[i].setAttribute('open', true);
+				}
+			}
+		}
+
+		if (openAll.open) {
+			openAll.childNodes[0].innerHTML = 'Open all sections';
+		} else {
+			openAll.childNodes[0].innerHTML = 'Close all sections';
+		}
+
+		return true;
 	}
 }
 
-addCloseButtonsToDetails();
+enableGuideControls();
